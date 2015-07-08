d940a4569dd8b030d9cef538f557bec94f52e3ff
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-07-08 15:02:46 -0400

    add methods to linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -2,26 +2,65 @@
 #include <stdio.h>
 
 struct node {
-  int val;
-  struct node *next;
+  int data;
+  struct node* next;
 };
 
-typedef struct node item;
+void print_list(struct node* head) {
+  struct node* curr = head;
+  while (curr) {
+    printf("%d\n", curr->data);
+    curr = curr->next;
+  }
+}
 
-int main()
-{
-  item *curr, *head;
-  int i;
+int print_length(struct node* head) {
+  struct node* curr = head;
+  int count = 0;
+  while (curr) {
+    curr = curr->next;
+    count++;
+  }
+  return count;
+}
+
+struct node* append(struct node* head, int data) {
+  if (!head) {
+    head->data = data;
+    head->next = NULL;
+  } else {
+    struct node* curr = head;
+    while (curr) {
+      if (curr->next) {
+        curr = curr->next;
+      } else {
+        curr = (struct node*)malloc(sizeof(struct node));
+        curr->data = data;
+        curr->next = NULL;
+        curr = curr->next;
+      }
+    }
+  }
+  return head;
+}
+
+struct node* push(struct node* head, int data) {
+  struct node *new = (struct node*)malloc(sizeof(struct node));
+  new->data = data;
+  new->next = head;
+  head = new;
+  return head;
+}
+
+int main() {
+  struct node *head, *curr;
   head = NULL;
-  for (i=1; i<10; i++) {
-    curr = (item*)malloc(sizeof(item));
-    curr->val = i;
+  int i;
+  for (i=0; i<10; i++) {
+    curr = (struct node*)malloc(sizeof(struct node));
+    curr->data = i;
     curr->next = head;
     head = curr;
   }
-  curr = head;
-  while (curr) {
-    printf("%d\n", curr->val);
-    curr = curr->next;
-  }
+  printf("%d\n", print_length(head));
 }
