12c02e8a7d9ef55b0bb8c126cde1c552f92c2141
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-07-08 20:17:06 -0400

    remove* methods should return the node data

--- linked_list.c
+++ linked_list.c
@@ -61,23 +61,26 @@ struct node* push(struct node* head, int data) {
   return head;
 }
 
-// remove data from the end
-struct node* remove_last(struct node* head) {
+// remove data from the end and return it
+int remove_last(struct node* head) {
   struct node* curr = head;
   while ((curr->next)->next) { // find the second to last node
     curr = curr->next;
   }
   struct node* last = curr->next;
   curr->next = NULL;
+  int data = last->data;
   free(last);
-  return head;
+  return data;
 }
 
-struct node* remove_first(struct node* head) {
-  struct node* first = head;
-  head = first->next;
-  free(first);
-  return head;
+
+// remove data from the beginning and return the new head and the data
+// this is wonky, I'm abusing the node struct but YOLO right?
+struct node remove_first(struct node* head) {
+  struct node values = { head->data, head->next };
+  free(head);
+  return values;
 }
 
 // test
@@ -86,14 +89,15 @@ int main() {
   int i;
   for (i=0; i<10; i++) {
     head = push(head, i);
+    head = append(head, i + 10);
   }
-  for (i=10; i<20; i++) {
-    head = append(head, i);
-  }
+  print_list(head);
   printf("length: %d\n", print_length(head));
+  printf("last: %d\n", remove_last(head));
   print_list(head);
-  head = remove_last(head);
-  print_list(head);
-  head = remove_first(head);
+  struct node pop_results = remove_first(head);
+  head = pop_results.next;
+  int first = pop_results.data;
+  printf("first: %d\n", first);
   print_list(head);
 }
